c41019ee978b781f5ed421252ea3e2ffc320f276
Habib Syuhada <habibsumedang@gmail.com>
2020-05-10 14:53:16 +0700

    .

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   constructor() {
     super();
     //Setting up global variable
-    global.socketlink = socketIOClient('https://sky-serverst.herokuapp.com/');
+    global.socketlink = socketIOClient('https://sky-serverst.herokuapp.com/:4001');
 
   }
   render() {
